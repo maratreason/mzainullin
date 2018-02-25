8bcf4d7c5da6f5a634715e5bd7fafb26c9d33ee9
maratreason <maratreason@mail.ru>
2018-02-25 13:27:41 +0300

    1.3. JavDoc. Code convetion.

---
+++ chapter_001/src/main/java/ru/mzainullin/Calculate.java
@@ -0,0 +1,22 @@
+﻿package ru.mzainullin;
+
+/**
+* Class Calculate решение задачи 001 
+* @author zainullin
+* @since 25.02.2018 
+*/
+
+public class Calculate {
+	public String echo (String value) {
+		return String.format("%s %s %s", value, value, value);
+	}
+
+/**
+* Main method.
+* @param args - args.
+*/
+	public static void main(String[] args) {
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo("yeah"));
+	}
+}
