46aa8c5b3c4913b756ffc70023b224045d323af1
maratreason <maratreason@mail.ru>
2018-03-02 20:37:17 +0300

    3.2. Максимум из двух чисел

--- chapter_001/src/test/java/ru/mzainullin/max/MaxTest.java
+++ chapter_001/src/test/java/ru/mzainullin/max/MaxTest.java
@@ -15,8 +15,8 @@ public class MaxTest {
 	@Test
 	public void whenFirstLessSecond() {
 		Max maxim = new Max();
-		int result = maxim.max(1, 2);
-		assertThat(result, is(2));
+		int result = maxim.max(8, 4);
+		assertThat(result, is(8));
 	}
 
 }
