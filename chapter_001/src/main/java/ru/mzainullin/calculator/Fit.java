59535584089560d94e9953e3fc5e2e5657e58c74
maratreason <maratreason@mail.ru>
2018-03-03 16:42:48 +0300

    3.3 Идеальный вес

--- chapter_001/src/main/java/ru/mzainullin/calculator/Fit.java
+++ chapter_001/src/main/java/ru/mzainullin/calculator/Fit.java
@@ -5,14 +5,16 @@ package ru.mzainullin.calculator;
 */
 public class Fit {
 
+	private static final double MAN_WEIGHT = 100;
+	private static final double WOMAN_WEIGHT = 110;
+	private static final double MULT_WEIGHT = 1.15;
+
 	/**
 	* Идеальный вес для мужщины.
 	* @param height Рост.
 	* @return идеальный вес.
 	*/
 	double manWeight(double height) {
-		final double MAN_WEIGHT = 100;
-		final double MULT_WEIGHT = 1.15;
 		return (height - MAN_WEIGHT) * MULT_WEIGHT;
 	}
 
@@ -22,8 +24,6 @@ public class Fit {
 	* @return идеальный вес.
 	*/
 	double womanWeight(double height) {
-		final double WOMAN_WEIGHT = 110;
-		final double MULT_WEIGHT = 1.15;
 		return (height - WOMAN_WEIGHT) * MULT_WEIGHT;
 	}
 
