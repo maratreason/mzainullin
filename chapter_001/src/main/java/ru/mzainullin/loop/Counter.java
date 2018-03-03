79e9428ab6c7dab1b93a17f0f0fcc2723120f4bb
maratreason <maratreason@mail.ru>
2018-03-03 15:25:54 +0300

    4.1 Подсчет суммы четных чисел в диапазоне

--- chapter_001/src/main/java/ru/mzainullin/loop/Counter.java
+++ chapter_001/src/main/java/ru/mzainullin/loop/Counter.java
@@ -7,7 +7,6 @@ package ru.mzainullin.loop;
 */
 public class Counter {
 
-	public int temp;
 	/**
 	* Метод должен вычислять сумму четныx чисел в диапазоне от start до finish;
 	* @param first
@@ -15,15 +14,14 @@ public class Counter {
 	* @return Сумма чисел переменной first. От числа first до числа finish.
 	*/
 	public int add(int start, int finish) {
-		
+		int temp = 0;
 		for (start = 0; start <= finish; start++) {
+
 			if (start % 2 == 0) {
 				temp += start;
 			}
-			
 		}
 		return temp;
-		
 	}
 
 }
