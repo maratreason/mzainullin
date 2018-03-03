cde15734eddf6995fb35bdad2a1d8f270922db13
maratreason <maratreason@mail.ru>
2018-03-03 09:14:58 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/main/java/ru/mzainullin/loop/Counter.java
@@ -0,0 +1,29 @@
+package ru.mzainullin.loop;
+
+/**
+* @author Marat Zainullin
+* @version 1.0
+* @since 0.1
+*/
+public class Counter {
+
+	public int temp;
+	/**
+	* Метод должен вычислять сумму четныx чисел в диапазоне от start до finish;
+	* @param first
+	* @param finish
+	* @return Сумма чисел переменной first. От числа first до числа finish.
+	*/
+	public int add(int start, int finish) {
+		
+		for (start = 0; start <= finish; start++) {
+			if (start % 2 == 0) {
+				temp += start;
+			}
+			
+		}
+		return temp;
+		
+	}
+
+}
---
+++ chapter_001/src/test/java/ru/mzainullin/loop/CounterTest.java
@@ -0,0 +1,16 @@
+package ru.mzainullin.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter counter = new Counter();
+        int result = counter.add(0, 10);
+        assertThat(result, is(30));
+    }
+}
