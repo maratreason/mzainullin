aa21040146ae667d5dffa6bc865169140065d24d
maratreason <maratreason@mail.ru>
2018-03-04 17:44:53 +0300

    4.2. Создать программу вычисляющую факториал.

--- chapter_001/src/main/java/ru/mzainullin/loop/Factorial.java
+++ chapter_001/src/main/java/ru/mzainullin/loop/Factorial.java
@@ -1,15 +1,19 @@
 package ru.mzainullin.loop;
 
+/**
+* @author Marat Zainullin
+* @version 1.0
+* @since 0.1
+*/
 public class Factorial {
 
 	public int calc(int n) {
-
 		int temp = 1;
 
 		for (int i = 1; i <= n; i++) {
 			temp *= i;
-		}
-		
+		}		
+
 		return temp;
 	}
 
