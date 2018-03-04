82e0a8563639806fde528caeb2fde122cc9f2d25
maratreason <maratreason@mail.ru>
2018-03-04 17:43:24 +0300

    3.4. Расстояние между точками в системе координат

--- chapter_001/src/main/java/ru/mzainullin/condition/Point.java
+++ chapter_001/src/main/java/ru/mzainullin/condition/Point.java
@@ -22,31 +22,10 @@ public class Point {
     * @return квадратный корень
 	*/
 	public double distanceTo(Point that) {
-//		Point a = this;
-//		Point b = that;
-
-//		int x1 = a.x;
-//		int y1 = a.y;
-//		int x2 = b.x;
-//		int y2 = b.y;
-//
-//		double result = Math.sqrt(
-//				Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
-//		);
-//
-//		return result;
 
 		return Math.sqrt(
 				Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
 		);
-	}
-
-	public static void main(String[] args) {
-		Point startPoint = new Point(0, 1);
-		Point endPoint = new Point(2, 18);
-
-		double result =  startPoint.distanceTo(endPoint);
-		System.out.println("Растояние между точками startPoint и endPoint: " + result);
 	}
 
 }
