2285f8c6cd350c1e7bee8158741b2599fdf6aae3
maratreason <maratreason@mail.ru>
2018-03-04 13:34:17 +0300

    4.1. Глупый бот

--- chapter_001/src/main/java/ru/mzainullin/condition/Triangle.java
+++ chapter_001/src/main/java/ru/mzainullin/condition/Triangle.java
@@ -12,50 +12,60 @@ public class Triangle {
 	private Point b;
 	private Point c;
 
-	public Triangle(Point aPoint, Point bPoint, Point cPoint) {
-		this.a = aPoint;
-		this.b = bPoint;
-		this.c = cPoint;
+	public Triangle(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
 	}
 
 	/**
 	* Метод вычисления полупериметра по длинам сторон.
+	* Формула.
+	*
+	* (ab + ac + bc) / 2
+	*
 	* @param ab расстояние между точками a b
 	* @param ac расстояние между точками a c
 	* @param bc расстояние между точками b c
 	* @return Периметр.
 	*/
+	
 	public double period(double ab, double ac, double bc) {
-		double result = (ab + ac + bc) / 2;
-		return result;
+		return (ab + ac + bc) / 2;
 	}
 
 	/**
 	* Метод вычисления площади.
-	* @return Вернуть прощадь, если треугольник существует или -1, если треугольника нет.
+	* @return Вернуть площадь, если треугольник существует или -1, если треугольника нет.
 	*/
+
 	public double area() {
-		double rsl = 2;
+		double rsl = -1;
 		double ab = this.a.distanceTo(this.b);
 		double ac = this.a.distanceTo(this.c);
 		double bc = this.b.distanceTo(this.c);
 		double p = this.period(ab, ac, bc);
+		double s;
 
 		if (this.exists(ab, ac, bc)) {
-			rsl = ab * ac * bc;
+			s = Math.sqrt(p * ((p-ab) * (p-ac) * (p-bc)));
+			rsl = s;
 		}
 		return rsl;
 	}
 
 	/**
-	* Метод проверяет можно ли построить треугольник с такими длинами сторон.
-	* @param ab Длина от точки a b. 
-	* @param ac Длина от точки a c.
-	* @param bc Длина от точки b c.
-	* @return 
-	*/
+	 * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+	 *
+	 * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+	 *
+	 * @param ab Длина от точки a b.
+	 * @param ac Длина от точки a c.
+	 * @param bc Длина от точки b c.
+	 * @return
+	 */
 	private boolean exists(double ab, double ac, double bc) {
-	   return false;
+		return true;
 	}
 
 }
--- chapter_001/src/test/java/ru/mzainullin/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/mzainullin/condition/TriangleTest.java
@@ -8,17 +8,22 @@ public class TriangleTest {
 
 	@Test
 	public void whenAreaSetThreePointsThenTriangleArea() {
-	 // создаем три объекта класса Point.
+
+		// Создаем три объекта класса Point.
 		Point a = new Point(0, 0);
-		Point b = new Point(0, 5);
-		Point c = new Point(3, 0);
-	 // Создаем объект треугольник и передаем в него объекты точек.
+		Point b = new Point(0, 2);
+		Point c = new Point(2, 0);
+
+	 	// Создаем объект треугольник и передаем в него объекты точек.
 		Triangle triangle = new Triangle(a, b, c);
-	 // Вычисляем площадь.
+
+	 	// Вычисляем площадь.
 		double result = triangle.area();
-	 // Задаем ожидаемый результат.
+
+	 	// Задаем ожидаемый результат.
 		double expected = 2D;
-	 //Проверяем результат и ожидаемое значение.
+
+	 	//Проверяем результат и ожидаемое значение.
 		assertThat(result, closeTo(expected, 0.1));
 	}
 }
