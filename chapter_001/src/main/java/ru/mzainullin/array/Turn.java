3c6ac0424ad21bf507a7eae07fcf6787dc0904e3
maratreason <maratreason@mail.ru>
2018-03-06 22:40:54 +0300

    5.2. Перевернуть массив.

---
+++ chapter_001/src/main/java/ru/mzainullin/array/Turn.java
@@ -0,0 +1,43 @@
+package ru.mzainullin.array;
+
+/**
+ * @author MaratZainullin
+ * @version 1.0
+ * @since 0.1
+ */
+public class Turn {
+
+    /**
+     * Метод делает реверс массива.
+     * @param array
+     * @return перевернутый массив.
+     */
+    public int[] back(int[] array) {
+
+        if (array == null || array.length <= 1) {
+            return null;
+        }
+
+        for(int i = 0; i < array.length / 2; i++) {
+
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+        /* for(int i = 1; i <= array.length; i++) {
+                System.out.println(array[array.length - i]);
+            }*/
+
+        return array;
+    }
+
+   /* public static void main(String[] args) {
+        int[] numbers = {3, 5, 6, 2, 1, 7, 8, 9};
+
+        Turn turn = new Turn();
+        int[] result = turn.back(numbers);
+
+    }*/
+
+}
---
+++ chapter_001/src/test/java/ru/mzainullin/array/TurnTest.java
@@ -0,0 +1,29 @@
+package ru.mzainullin.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author MaratZainullin
+ * @version 1.0
+ * @since 0.1
+ */
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+
+        int[] numbers = {1, 5, 7, 4, 3, 7, 8, 9};
+        Turn turn = new Turn();
+        int[] result = turn.back(numbers);
+        int[] expectArray = {9, 8, 7, 3, 4, 7, 5, 1};
+        assertThat(result, is(expectArray));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+    }
+
+}
