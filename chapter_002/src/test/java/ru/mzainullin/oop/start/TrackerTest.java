7963ba8c04ab9e0f3a575023ead1de7bdba0232e
maratreason <maratreason@mail.ru>
2018-04-06 16:31:39 +0300

    1. Реализовать класс tracker

--- chapter_002/src/test/java/ru/mzainullin/oop/start/TrackerTest.java
+++ chapter_002/src/test/java/ru/mzainullin/oop/start/TrackerTest.java
@@ -10,14 +10,67 @@ import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
 
-    // @Test
-    // public void whenAddNewItemThenTrackerHasSameItem() {
-    //     Tracker tracker = new Tracker();
-    //     Item item = new Item("test1","testDescription",123L);
-    //     tracker.add(item);
-    //     assertThat(tracker.findAll()[0], is(item));
-    // }
+     @Test
+     public void whenAddNewItemThenTrackerHasSameItem() {
+         Tracker tracker = new Tracker();
+         Item item = new Item("test1","testDescription",123L);
+         tracker.add(item);
+         assertThat(tracker.findAll()[0], is(item));
+     }
 
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+
+        Item previous = new Item("test1", "testDescription", 123L);
+        tracker.add(previous);
+
+        // Создаем новую заявку.
+        Item next = new Item("test2", "testDescription2", 1234L);
+        next.setId(previous.getId());
+
+        // Удаляем заявку в трекере.
+        tracker.delete(previous.getId());
+        // is() ждет строку, поэтому пришлось добавить эту переменную
+        String strNull = null;
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(previous.getId()), is(strNull));
+    }
+
+    @Test
+    public void whenFindItemByNameAndReturnItem() {
+
+        Tracker tracker = new Tracker();
+
+        // Создаем заявку.
+        Item previous = new Item("Smart", "testDescription", 123L);
+        tracker.add(previous);
+
+        // Создаем новую заявку.
+        Item next = new Item("test2", "testDescription2", 1234L);
+        next.setId(previous.getId());
+
+        // Проверяем, что заявка с таким id имеет имя Smart.
+        assertThat(tracker.findById(previous.getId()).getName(), is("Smart"));
+    }
+
+    @Test
+    public void whenFindItemByIdAndReturnItemName() {
+
+        Tracker tracker = new Tracker();
+
+        // Создаем заявку.
+        Item previous = new Item("Smart", "testDescription", 123L);
+        tracker.add(previous);
+
+        // Создаем новую заявку.
+        Item next = new Item("test2", "testDescription2", 1234L);
+        next.setId(previous.getId());
+
+        // Проверяем, что заявка с таким id имеет имя Smart.
+        // Проверить id невозможно, т.к. оно динамическое. Поэтому поставил в ожидании то что и в assert.
+        assertThat(tracker.findById(previous.getId()).getId(), is(tracker.findById(previous.getId()).getId()));
+    }
 
     @Test
     public void whenReplaceNameThenReturnNewName() {
@@ -28,37 +81,15 @@ public class TrackerTest {
         // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
 
-
         // Создаем новую заявку.
         Item next = new Item("test2", "testDescription2", 1234L);
         // Проставляем старый id из previous, который был сгенерирован выше.
         next.setId(previous.getId());
 
-
         // Обновляем заявку в трекере.
         tracker.replace(previous.getId(), next);
         // Проверяем, что заявка с таким id имеет новые имя test2.
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
-
-
-        // Найти все заявки в трекере.
-        tracker.findAll();
-        // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
-
-
-        // Найти заявку по имени в трекере.
-        tracker.findByName(previous.getName());
-        // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
-
-
-        // Удаляем заявку в трекере.
-        tracker.delete(previous.getId());
-        // is() ждет строку, поэтому пришлось добавить эту переменную
-        String strNull = null;
-        // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findById(previous.getId()), is(strNull));
     }
 
     @Test
@@ -74,10 +105,7 @@ public class TrackerTest {
         // Найти все заявки в трекере.
         tracker.findAll();
 
-        for (Item item : tracker.findAll()) {
-            System.out.println(item);
-        }
         // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findAll().toString(), is("Задача"));
+        assertThat(tracker.findAll()[0].getName(), is("Задача"));
     }
 }
