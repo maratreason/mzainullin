7a4a4788ee94c439c83198d626dd9683bb1f0b27
maratreason <maratreason@mail.ru>
2018-04-27 15:23:54 +0300

    1. Рефакторинг - Перенести общие методы в абстрактный класс

--- chapter_002/src/main/java/ru/mzainullin/oop/start/MenuTracker.java
+++ chapter_002/src/main/java/ru/mzainullin/oop/start/MenuTracker.java
@@ -1,5 +1,108 @@
 package ru.mzainullin.oop.start;
 
+import ru.mzainullin.oop.models.Item;
+import ru.mzainullin.oop.models.Task;
+
+class ShowAllItems extends BaseAction {
+
+    public ShowAllItems(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        for (Item item : tracker.findAll()) {
+            if (item != null) {
+                System.out.println(String.format("%s. %s", item.getId(), item.getName()));
+            }
+        }
+    }
+}
+
+class AddItem extends BaseAction {
+
+    public AddItem(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        String name = input.ask("Enter the new task name: ");
+        String desc = input.ask("Enter the new task desc: ");
+        tracker.add(new Task(name, desc));
+    }
+
+}
+
+class DeleteItem extends BaseAction {
+
+    public DeleteItem(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        String id = input.ask("Enter task's id you want to delete: ");
+        for (Item item : tracker.findAll()) {
+            if (item != null && item.getId().equals(id)) {
+                tracker.delete(item.getId());
+            }
+        }
+    }
+}
+
+class EditItem extends BaseAction {
+
+    public EditItem(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        String id = input.ask("Enter task's id: ");
+        String name = input.ask("Enter the new task name: ");
+        String desc = input.ask("Enter the new task desc: ");
+        Task task = new Task(name, desc);
+        task.setId(id);
+        tracker.edit(task);
+    }
+}
+
+class FindById extends BaseAction {
+
+    public FindById(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        String id = input.ask("Enter task's id: ");
+        for (Item item : tracker.findAll()) {
+            if (item.getId().equals(id)) {
+                System.out.println(item.getId() + " " + item.getName() + " " + item.getDescription());
+            }
+        }
+    }
+}
+
+class FindByName extends BaseAction {
+
+    public FindByName(int key, String name) {
+        super(key, name);
+    }
+
+    public void execute(Input input, Tracker tracker) {
+        String name = input.ask("Enter task's name: ");
+        for (Item item : tracker.findAll()) {
+            if (item.getName().equals(name)) {
+                System.out.println(item.getId() + " " + item.getName() + " " + item.getDescription());
+            }
+        }
+    }
+}
+
+class Exit extends BaseAction {
+    public Exit(int key, String name) {
+        super(key, name);
+    }
+    public void execute(Input input, Tracker tracker) {}
+}
+
 /**
  * @author Marat Zainullin
  * @version 1.2
