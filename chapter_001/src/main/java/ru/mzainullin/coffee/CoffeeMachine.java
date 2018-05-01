8440793d9f62e8c5671ea61d318120ced58816ee
maratreason <maratreason@mail.ru>
2018-05-01 16:56:28 +0300

    Кофемашина.

---
+++ chapter_001/src/main/java/ru/mzainullin/coffee/CoffeeMachine.java
@@ -0,0 +1,46 @@
+package ru.mzainullin.coffee;
+
+/**
+ * @author Marat Zainullin
+ * @version 1.0
+ * @since 01.05.2018
+ */
+public class CoffeeMachine {
+
+    int[] coins = {1, 2, 5, 10};
+
+    int[] changes(int value, int price) {
+
+        int position = 0;
+        int summ = 0;
+        int backMoney = value - price;
+        int[] returnMoney = new int[100];
+
+        for (int i = 0; i < value; i++) {
+            if(coins[3] <= backMoney) {
+                returnMoney[position++] = coins[3];
+                backMoney -= coins[3];
+            } else if (coins[2] <= backMoney && coins[3] > backMoney) {
+                returnMoney[position++] = coins[2];
+                backMoney -= coins[2];
+            } else if (coins[1] <= backMoney && coins[2] > backMoney) {
+                returnMoney[position++] = coins[1];
+                backMoney -= coins[1];
+            } else if (coins[0] <= backMoney && coins[1] > backMoney) {
+                returnMoney[position++] = coins[0];
+                backMoney -= coins[0];
+            }
+        }
+
+        for (int money : returnMoney) {
+            if(money == 0) {
+                break;
+            } else {
+                System.out.print(money + " ");
+                summ += money;
+            }
+        }
+        System.out.println("\nУважаемый клиент, вот ваша сдача: " + summ + " евро.");
+        return returnMoney;
+    }
+}
---
+++ chapter_001/src/test/java/ru/mzainullin/coffee/CoffeeTest.java
@@ -0,0 +1,20 @@
+package ru.mzainullin.coffee;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Marat Zainullin (maratreason@mail.ru)
+ * @version 1.0
+ * @since 01.05.2018
+ */
+public class CoffeeTest {
+
+    @Test
+    public void whenPaidTheMoneyAndReturnBackMoney() {
+        CoffeeMachine coffee = new CoffeeMachine();
+        assertThat(coffee.changes(98, 35), is("10 10 10 10 10 10 2 1"));
+    }
+
+}
