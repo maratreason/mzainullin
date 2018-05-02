28ddaa0f68b08fe99f75895f93b0a5415700f9ba
maratreason <maratreason@mail.ru>
2018-05-02 16:35:52 +0300

    2. Очередь с приоритетом на LinkedList

--- chapter_001/src/main/java/ru/mzainullin/search/PriorityQueue.java
+++ chapter_001/src/main/java/ru/mzainullin/search/PriorityQueue.java
@@ -14,12 +14,22 @@ public class PriorityQueue {
      */
     public void put(Task task) {
         //TODO добавить вставку в связанный список.
-        if (task.getPriority() == 5) {
-            this.tasks.add(task);
-        } else if (task.getPriority() == 1) {
-            this.tasks.add(task);
-        } else if (task.getPriority() == 3) {
-            this.tasks.add(task);
+//        if (task.getPriority() == 5) {
+//            this.tasks.addLast(task);
+//        } else if (task.getPriority() == 1) {
+//            this.tasks.addFirst(task);
+//        } else if (task.getPriority() == 3) {
+//            this.tasks.add(task);
+//        }
+
+        for (int index = 0; index < tasks.size(); index++) {
+            if (task.getPriority() <= tasks.get(index).getPriority()) {
+                tasks.addFirst(task);
+                break;
+            } else {
+                tasks.add(index, task);
+                break;
+            }
         }
     }
 
@@ -30,23 +40,27 @@ public class PriorityQueue {
     public static void main(String[] args) {
 
         LinkedList<Task> tasks1 = new LinkedList<>();
+//        tasks1.add(new Task("1 Задача низкого приоритета", 5));
+//        tasks1.add(new Task("2 Задача высокого приоритета", 1));
+//        tasks1.add(new Task("3 Задача высокого приоритета", 1));
+//        tasks1.add(new Task("4 Задача низкого приоритета", 5));
+//        tasks1.add(new Task("5 Задача среднего приоритета", 3));
 
         PriorityQueue queue = new PriorityQueue();
         queue.put(new Task("1 Задача низкого приоритета", 5));
-        tasks1.add(queue.take());
         queue.put(new Task("2 Задача высокого приоритета", 1));
-        tasks1.add(queue.take());
         queue.put(new Task("3 Задача высокого приоритета", 1));
-        tasks1.add(queue.take());
         queue.put(new Task("4 Задача низкого приоритета", 5));
-        tasks1.add(queue.take());
         queue.put(new Task("5 Задача среднего приоритета", 3));
 
-        tasks1.add(queue.take());
+        for (int index = 0; index < tasks1.size(); index++) {
+            tasks1 = queue.take();
+//            tasks1.add(index, queue.take());
+        }
 
         for (Task task : tasks1) {
-            System.out.println(task.getPriority());
+            System.out.println(task.getPriority() + " : " + task.getDesc());
         }
-    }
 
+    }
 }
