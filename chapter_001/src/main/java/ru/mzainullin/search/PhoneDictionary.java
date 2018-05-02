0b958e35f78474b9d232df6a07b82da9b226d9c2
maratreason <maratreason@mail.ru>
2018-05-02 13:22:59 +0300

    1. Телефонный справочник на базе ArrayList

--- chapter_001/src/main/java/ru/mzainullin/search/PhoneDictionary.java
+++ chapter_001/src/main/java/ru/mzainullin/search/PhoneDictionary.java
@@ -22,7 +22,7 @@ public class PhoneDictionary {
         for(Person person : persons) {
             if (person.getName().contains(key) || person.getSurname().contains(key) ||
                 person.getPhone().contains(key) || person.getAddress().contains(key)) {
-                result = persons;
+                result.add(person);
             }
         }
         return result;
