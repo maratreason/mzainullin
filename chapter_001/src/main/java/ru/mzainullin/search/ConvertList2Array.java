6dd2f7a85b21230b7c826d2a22a0d06b6dbd865a
maratreason <maratreason@mail.ru>
2018-05-03 16:06:05 +0300

    3. Конвертация ArrayList в двухмерный массив

--- chapter_001/src/main/java/ru/mzainullin/search/ConvertList2Array.java
+++ chapter_001/src/main/java/ru/mzainullin/search/ConvertList2Array.java
@@ -1,5 +1,6 @@
 package ru.mzainullin.search;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,14 +11,43 @@ import java.util.List;
  */
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        for (int i = 0; i != list.size(); i++) {
-            if(!(list.size() % rows == 0)) {
-                list.add(0);
-                break;
+
+        Integer newArray[] = new Integer [list.size()] ;
+        newArray = list.toArray(newArray);
+        int cells = rows;
+        for (int arr : newArray) {
+            if (arr <= rows) {
+                cells = arr;
             }
         }
-        int cells = list.size();
+
         int[][] array = new int[rows][cells];
         return array;
+    }
+
+    public static void main(String[] args) {
+        ConvertList2Array convert = new ConvertList2Array();
+        List<Integer> lists = new ArrayList<>();
+        lists.add(1);
+        lists.add(2);
+        lists.add(3);
+        lists.add(4);
+        lists.add(5);
+        lists.add(6);
+        lists.add(7);
+
+        Integer ia[] = new Integer [lists.size()] ;
+        ia = lists.toArray(ia);
+
+        System.out.println( "Coдepжимoe списочного массива ia : " + Arrays.toString(ia));
+
+        System.out.println("---");
+        int[][] arr2 = convert.toArray(Arrays.asList(ia), 3);
+
+        for(int[] arr3 : arr2) {
+            System.out.print(Arrays.toString(arr3));
+        }
+
+
     }
 }
