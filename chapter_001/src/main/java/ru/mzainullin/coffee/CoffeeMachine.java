491fc02e5e55b29534207d4afbf9ecb627622a94
maratreason <maratreason@mail.ru>
2018-05-16 16:08:24 +0300

    Кофемашина

--- chapter_001/src/main/java/ru/mzainullin/coffee/CoffeeMachine.java
+++ chapter_001/src/main/java/ru/mzainullin/coffee/CoffeeMachine.java
@@ -1,6 +1,8 @@
 package ru.mzainullin.coffee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
 
 /**
@@ -14,49 +16,23 @@ public class CoffeeMachine {
 
     int[] changes(int value, int price) {
 
-        int position = 0;
-        int summ = 0;
         int backMoney = value - price;
-        int[] returnMoney = new int[100];
+        int index = coins.length - 1;
+        List<Integer> lists = new ArrayList<>();
 
-//        for (int i = 0; i < value; i++) {
-//            if(coins[3] <= backMoney) {
-//                returnMoney[position++] = coins[3];
-//                backMoney -= coins[3];
-//            } else if (coins[2] <= backMoney && coins[3] > backMoney) {
-//                returnMoney[position++] = coins[2];
-//                backMoney -= coins[2];
-//            } else if (coins[1] <= backMoney && coins[2] > backMoney) {
-//                returnMoney[position++] = coins[1];
-//                backMoney -= coins[1];
-//            } else if (coins[0] <= backMoney && coins[1] > backMoney) {
-//                returnMoney[position++] = coins[0];
-//                backMoney -= coins[0];
-//            }
-//        }
-
-        for (int coin : coins) {
-            while (value - coin > 0) {
-                OptionalInt max = Arrays.stream(coins).max();
-                int maxCoin = max.getAsInt();
-                returnMoney[position++] = maxCoin;
-//                if (backMoney < maxCoin) {
-//
-//                }
-                backMoney -= coin;
-                value -= coin;
+        while (backMoney > 0) {
+            while (backMoney >= coins[index]) {
+                lists.add(coins[index]);
+                backMoney -= coins[index];
             }
+            index--;
         }
+        int[] result = new int[lists.size()];
+        for (int i = 0; i != result.length; i++) {
+            result[i] = lists.get(i);
+        }
+        System.out.println(String.format("\nУважаемый клиент, вот ваша сдача состоит из: %s монет.", Arrays.toString(result)));
 
-        for (int money : returnMoney) {
-            if (money == 0) {
-                break;
-            } else {
-                System.out.print(money + " ");
-                summ += money;
-            }
-        }
-        System.out.println("\nУважаемый клиент, вот ваша сдача: " + summ + " евро.");
-        return returnMoney;
+        return result;
     }
 }
