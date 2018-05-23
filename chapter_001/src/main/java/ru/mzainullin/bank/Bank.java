5451481c4d266a055d86fd9d643baecf4364afde
maratreason <maratreason@mail.ru>
2018-05-23 12:08:22 +0300

    Банковские переводы.

--- chapter_001/src/main/java/ru/mzainullin/bank/Bank.java
+++ chapter_001/src/main/java/ru/mzainullin/bank/Bank.java
@@ -14,14 +14,13 @@ public class Bank {
      */
     Map<User, List<Account>> userListMap = new TreeMap<>();
 
-    List<Account> accounts;
 
     /**
      * Метод добавления пользователя.
      * @param user - новый пользователь.
      */
     public void addUser(User user) {
-        this.userListMap.putIfAbsent(user, accounts);
+        this.userListMap.putIfAbsent(user,  new ArrayList<>());
     }
 
 
@@ -92,58 +91,25 @@ public class Bank {
         boolean trans = false;
         List<Account> srcList = getUserAccounts(srcPassport);
         List<Account> dstList = getUserAccounts(destPassport);
+
         for (Account account : srcList) {
             if (account.getRequisites().equals(srcRequisite)) {
                 src = true;
                 srcList = Arrays.asList(account);
             }
         }
+
         for (Account account : dstList) {
             if (account.getRequisites().equals(destRequisite)) {
                 dst = true;
                 dstList = Arrays.asList(account);
             }
         }
-        if ((src && dst) && (srcList.get(0).transfer(dstList.get(0), amount))) {
 
+        if ((src && dst) && (srcList.get(0).transfer(dstList.get(0), amount))) {
             trans = true;
         }
         return trans;
     }
 
 }
-
-
-
-/*
-Реализовать коллекцию Map для банка
-
-Необходимо создать класс User с полями name, passport.
-
-Добавить методы eqauls() hashCode()
-
-Необходимо создать класс Account с полями value (кол-во денег), requisites (реквизиты счёта) - это банковский счёт.
-
-Реализовать коллекцию Map <User, List<Account>>, обозначающую что у каждого пользователя может быть список банковских счетов.
-
-Необходимо реализовать возможность перечислять деньги, как с одного счёта User на другой счёт того же User, так и на счёт другого User.
-
-В программе должны быть методы:
-
-public void addUser(User user) {} - добавление пользователя.
-
-public void deleteUser(User user) {} - удаление пользователя.
-
-public void addAccountToUser(String passport, Account account) {} - добавить счёт пользователю.
-
-public void deleteAccountFromUser(String passport, Account account) {} - удалить один счёт пользователя.
-
-public List<Accounts> getUserAccounts (String passport) {} - получить список счетов для пользователя.
-
-public boolean transferMoney (String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount)
- - метод для перечисления денег с одного счёта на другой счёт:
-если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят) должен вернуть false.
-
-Посмотрите на методы Map.putIfAbsent и List.indexOf, как их можно применить в этом задании.
-
- */
--- chapter_001/src/test/java/ru/mzainullin/bank/BankAppTest.java
+++ chapter_001/src/test/java/ru/mzainullin/bank/BankAppTest.java
@@ -1,9 +1,10 @@
 package ru.mzainullin.bank;
 
-import org.junit.Test;
-
 import java.util.*;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,36 +14,135 @@ import static org.junit.Assert.assertThat;
  */
 public class BankAppTest {
 
-    @Test
-    public void whenOneAccountPadiToNextAccount() {
-        Bank bank = new Bank();
+    private static Bank bank;
+    private static User userOne;
+    private static User userTwo;
+    private static Account accountOne;
+    private static Account accountTwo;
+    private static Account accountThree;
 
-        Map<User, List<Account>> userMoney = new TreeMap<>();
 
-        List<Account> firstAccount = new ArrayList<>();
-        firstAccount.add(new Account(30000.0, "94543654623"));
-        firstAccount.add(new Account(40000.0, "94543654623"));
-
-        List<Account> secondAccount = new ArrayList<>();
-        secondAccount.add(new Account(50000.0, "11113654623"));
-        secondAccount.add(new Account(45000.0, "11113654623"));
-
-        userMoney.put(new User("Irina", "8932341312"), firstAccount);
-        userMoney.put(new User("Ivan", "1152341314"), secondAccount);
-
-        for (User user : userMoney.keySet()) {
-            System.out.println(String.format("%s : %s", user, userMoney.get(user)));
-        }
-
-        assertThat(bank.transferMoney(
-                "1152341314", "11113654623",
-                "8932341312", "94543654623", 25000.0),
-                is(userMoney.get(new User("Irina", "8932341312"))));
-
-        for (User user : userMoney.keySet()) {
-            System.out.println(String.format("%s : %s", user, userMoney.get(user)));
-        }
-
+    @Before
+    public void beforeTest() {
+        bank = new Bank();
+        userOne = new User("Alexander", "1234");
+        userTwo = new User("Marat", "5678");
+        bank.addUser(userOne);
+        bank.addUser(userTwo);
+        accountOne = new Account(100.0, "Alexander1234");
+        accountTwo = new Account(50.0, "Marat5678");
     }
+
+
+    @Test
+    public void whenAddAccountToUserThenResultNewAccount() {
+        bank.addAccountToUser("1234", accountOne);
+        assertThat(bank.getUserAccounts("1234"), Matchers.is(Arrays.asList(accountOne)));
+    }
+
+
+    @Test
+    public void whenDeleteAccountFromUser() {
+        bank.addAccountToUser("1234", accountOne);
+        bank.addAccountToUser("5678", accountTwo);
+        bank.addUser(new User("newUser", "9998"));
+        bank.addAccountToUser("9998", new Account(1500.0, "newUser9998"));
+
+        for (List list  : bank.userListMap.values()) {
+            for (Object obj : list) {
+                System.out.println(obj);
+            }
+        }
+        System.out.println();
+
+        bank.deleteAccountFromUser("5678", accountTwo);
+        assertThat(bank.getUserAccounts("1234"), Matchers.is(Arrays.asList(accountOne)));
+
+        for (List list  : bank.userListMap.values()) {
+            for (Object obj : list) {
+                System.out.println(obj);
+            }
+        }
+    }
+
+
+    @Test
+    public void whenAddUserToBank() {
+
+        System.out.println("\n--- Список пользователей до добавления ---");
+        for (User user  : bank.userListMap.keySet()) {
+            System.out.println(String.format("%s, %s", user.getName(), user.getPassport()));
+        }
+
+        bank.addUser(new User("Ivan", "889"));
+        accountThree = new Account(90.0, "Ivan889");
+        bank.addAccountToUser("889", accountThree);
+
+        assertThat(bank.getUserAccounts("889"), Matchers.is(Arrays.asList(accountThree)));
+
+        System.out.println("\n--- Новый пользователь добавлен ---");
+        for (User user  : bank.userListMap.keySet()) {
+            System.out.println(String.format("%s, %s", user.getName(), user.getPassport()));
+        }
+    }
+
+
+    @Test
+    public void whenDeleteUserFromBank() {
+
+        bank.addAccountToUser("1234", accountOne);
+        bank.addAccountToUser("5678", accountTwo);
+
+        for (User user  : bank.userListMap.keySet()) {
+            System.out.println(String.format("%s, %s", user.getName(), user.getPassport()));
+        }
+
+        bank.deleteUser(userTwo);
+        assertThat(bank.getUserAccounts(userOne.getPassport()), Matchers.is(Arrays.asList(accountOne)));
+
+        System.out.println("\n--- После удаления пользователя ---");
+        for (User user  : bank.userListMap.keySet()) {
+            System.out.println(String.format("%s, %s", user.getName(), user.getPassport()));
+        }
+    }
+
+
+    @Test
+    public void whenTransferedMoneyFromFirstAccountToSecondAccount() {
+
+        bank.addAccountToUser("1234", accountOne);
+        bank.addAccountToUser("5678", accountTwo);
+
+        System.out.println("--- Счета до транзакции ---");
+        for (List list  : bank.userListMap.values()) {
+            for (Object obj : list) {
+                System.out.println(obj);
+            }
+        }
+
+        bank.transferMoney("5678", "Marat5678", "1234", "Alexander1234", 30.0);
+        assertThat(bank.getUserAccounts("1234"), is(Arrays.asList(accountOne)));
+
+        System.out.println("\n--- Счета после транзакции ---");
+        for (List list  : bank.userListMap.values()) {
+            for (Object obj : list) {
+                System.out.println(obj);
+            }
+        }
+    }
+
+
+    @Test
+    public void whenShowAllUserAccounts() {
+        bank.addAccountToUser("1234", accountOne);
+        bank.addAccountToUser("5678", accountTwo);
+
+        bank.getUserAccounts("5678");
+        assertThat(bank.getUserAccounts(userTwo.getPassport()), Matchers.is(Arrays.asList(accountTwo)));
+
+        bank.getUserAccounts("1234");
+        assertThat(bank.getUserAccounts(userOne.getPassport()), Matchers.is(Arrays.asList(accountOne)));
+    }
+
 
 }
