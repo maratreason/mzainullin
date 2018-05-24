bc5da3bc6d4960806f1545646343c12e60ad03c4
maratreason <maratreason@mail.ru>
2018-05-24 10:21:31 +0300

    5.1.3. Создать итератор простые числа.

--- chapter_003/src/main/java/ru.mzainullin/arrayIterator/PrimeIterator.java
+++ chapter_003/src/main/java/ru.mzainullin/arrayIterator/PrimeIterator.java
@@ -11,16 +11,45 @@ public class PrimeIterator implements Iterator {
     private int[] arr;
     private int position = 0;
 
+    public PrimeIterator() {}
+
     public PrimeIterator(final int[] numbers) {
         this.arr = numbers;
     }
+
+
+    /**
+     * Метод вычисления простых чисел.
+     * @param n - входящий параметр
+     * @return - простое число: true / false.
+     */
+    public boolean primes(int n) {
+        boolean isPrime = true;
+        if (n < 2) {
+            isPrime =  false;
+        } else if (n == 2) {
+            isPrime = true;
+        } else if (n % 2 == 0) {
+            isPrime = false;
+        }
+        // n > 1 is odd
+        int d = 3;
+        while(d * d <= n) {
+            if (n % d == 0) {
+                isPrime = false;
+            }
+            d += 2;
+        }
+        return isPrime;
+    }
+
 
     @Override
     public boolean hasNext() {
         // it.hasNext() - возвращает true, только если в массиве есть простые числа перед указателем.
         boolean isTrue = false;
         for (int index = 0; index != arr.length; index++) {
-            if (arr[index] > 0 && arr[index] % 2 == 0) {
+            if (arr[index] > 0 && primes(arr[index]) == true) {
                 this.position++;
                 isTrue = true;
                 break;
@@ -30,6 +59,7 @@ public class PrimeIterator implements Iterator {
         }
         return isTrue;
     }
+
 
     @Override
     public Object next() {
@@ -41,80 +71,4 @@ public class PrimeIterator implements Iterator {
         return arrPos;
     }
 
-    /*
-    * boolean b = true;
-        for (int P = 1; P <= 'число'; P++) {
-            for (int i = 1; i < P; i++)
-            {
-                if (P % i == 0){
-                    b = false;
-                }
-            System.out.println(P);}
-        }
-
-
-        или так:
-        public static boolean checkSimple(int i){
-            if (i<=1)
-                return false;
-            else if (i <=3)
-                return true;
-            else if (i%2==0 || i %3 ==0)
-                return false;
-            int n = 5;
-            while (n*n <=i){
-                if (i % n ==0 || i % (n+2) == 0)
-                    return false;
-            n=n+6;
-        }
-        return true;
-    }
-    *
-    *
-    *
-    *
-    *
-    * Или так:
-    * boolean isPrime=true;
-
-	int n=5;
-	for(int i=2;i<n;i++){
-         if((n%i)==0){
-            isPrime=false;
-            continue;
-          }
-        }
-    if(isPrime) System.out.println(n+" Prostoe");
-    else System.out.println("NE prostoe");
-
-
-
-    так:
-    public static boolean isPrime(long n) {
-		if (n < 0) return false; // negatives are not primes.
-		if (n == 0) return false; // 0 is not a prime
-		if (n == 1) return false; // 1 is not a prime
-		if (n < 4) return true; // 2 and 3 are primes
-		if (n % 2 == 0) return false; // filter out even numbers
-		if (n < 9) return true; // we already excluded 4, 6, 8.
-		if (n % 3 == 0) return false; // filter out numbers % 3
-
-		// if we can't find divider from 0 to r, then n is prime
-		long r = (long) Math.floor(Math.sqrt(n));
-
-		// All primes greater than 3 cab be written in 6k +/- 1
-		long f = 5;
-		while (f <= r) {
-			if (n % f == 0) return false;
-			if (n % (f + 2) == 0) return false;
-			f += 6;
-		}
-
-		return true;
-	}
-
-
-
-
-    * */
 }
