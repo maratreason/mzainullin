c2d4d2ce948ed2c65b0b703896603ff36dc0221b
maratreason <maratreason@mail.ru>
2018-05-24 15:26:54 +0300

    5.1.4. Создать convert(Iterator<Iterator>)

---
+++ chapter_003/src/main/java/ru.mzainullin/arrayIterator/Converter.java
@@ -0,0 +1,52 @@
+package ru.mzainullin.arrayIterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Marat Zainullin
+ * @since 24.05.2018
+ */
+public class Converter {
+
+    Iterator<Iterator<Integer>> iter;
+    private Iterator<Integer> currentIterator = null;
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        this.iter = it;
+
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                selectCurrentIterator();
+                return (currentIterator != null && currentIterator.hasNext());
+            }
+
+            @Override
+            public Integer next() {
+                selectCurrentIterator();
+                if (currentIterator == null) {
+                    throw new NoSuchElementException();
+                }
+                return currentIterator.next();
+            }
+
+            private void selectCurrentIterator() {
+                if (currentIterator != null && currentIterator.hasNext()) {
+                    return;
+                }
+                currentIterator = null;
+                while (iter.hasNext()) {
+                    Iterator<Integer> nextIterator = iter.next();
+                    if (nextIterator.hasNext()) {
+                        currentIterator = nextIterator;
+                        break;
+                    }
+                }
+            }
+
+        };
+    }
+
+}
