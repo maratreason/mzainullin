5b8206c45e010836bd2c482a19124c1e8e1dc381
maratreason <maratreason@mail.ru>
2018-05-24 16:38:44 +0300

    5.2.1. Реализовать SimpleArray<T>

---
+++ chapter_003/src/main/java/ru.mzainullin/iterators/generics/SimpleArray.java
@@ -0,0 +1,33 @@
+package ru.mzainullin.iterators.generics;
+
+
+/**
+ * @author Marat Zainullin
+ * @since 24.05.2018
+ */
+public class SimpleArray<T> {
+
+    add(T model){
+
+    }
+
+    set(int index, T model){
+
+    }
+
+    delete(int index){
+
+    }
+
+    get(int index){
+
+    }
+
+}
+
+/*
+* сделать универсальную обертку над массивом.
+* Так же, добавьте Iterable<T>.
+* Объект должен принимать количество ячеек. Структура не должна быть динамической.
+* Если идет переполнение надо выкинуть ошибку.
+* */
---
+++ chapter_003/src/test/java/ru/mzainullin/iterators/generics/SimpleArrayTest.java
@@ -0,0 +1,19 @@
+package ru.mzainullin.iterators.generics;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Marat Zainullin
+ * @since 24.05.2018
+ */
+public class SimpleArrayTest {
+
+    @Test
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(result, is());
+    }
+
+}
