a88277c571dd101042882a0d5939cf1a23b441c2
maratreason <maratreason@mail.ru>
2018-05-26 18:51:17 +0300

    5.2.2. Реализовать Store<T extends Base>

--- chapter_003/src/main/java/ru.mzainullin/iterators/generics/UserStore.java
+++ chapter_003/src/main/java/ru.mzainullin/iterators/generics/UserStore.java
@@ -7,29 +7,53 @@ package ru.mzainullin.iterators.generics;
 public class UserStore<T extends User> implements Store<T> {
 
     private T[] userArray;
+    private int position = 0;
 
     public UserStore(T[] arr) {
         this.userArray = arr;
     }
 
+    /**
+     * Метод для добавления модели
+     * @param model - модель для добавления
+     */
     @Override
     public void add(T model) {
-
+        this.userArray[position++] = model;
     }
 
+    /**
+     * Метод для замены одной модели на другую
+     * @param id - id модели для замены
+     * @param model - новая модель.
+     * @return - успешно заменили модель (true / false)
+     */
     @Override
     public boolean replace(String id, T model) {
         for (int index = 0; index != this.userArray.length; index++) {
-            if (id.equals(this.userArray[index])) {
+            if (id.equals(this.userArray[index].getId())) {
                 this.userArray[index] = model;
             }
         }
-        return false;
+        return true;
     }
 
+    /**
+     * Метод удаления модели
+     * @param id - id удаляемой модели
+     * @return - новый уменьшенный массив
+     */
     @Override
     public boolean delete(String id) {
-        return false;
+        for (int index = 0; index != this.userArray.length; index++) {
+            if (id.equals(this.userArray[index].getId())) {
+                Object[] tmp = new Object[this.userArray.length - 1];
+                System.arraycopy(this.userArray, 0, tmp, 0, index);
+                System.arraycopy(this.userArray, index + 1, tmp, index, this.userArray.length - index - 1);
+                this.userArray = (T) tmp;
+            }
+        }
+        return true;
     }
 
     @Override
