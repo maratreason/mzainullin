0fabc9a614a865eb27e764cdccd0d062cbce8a0e
maratreason <maratreason@mail.ru>
2018-06-06 16:45:06 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack и Queue.

--- chapter_003/src/main/java/ru.mzainullin/iterators/list/SimpleQueue.java
+++ chapter_003/src/main/java/ru.mzainullin/iterators/list/SimpleQueue.java
@@ -8,27 +8,16 @@ import java.util.NoSuchElementException;
  * @version 1.0
  * @since 04.06.2018
  */
-public class SimpleQueue<E> implements Iterable<E> {
+public class SimpleQueue<E> implements SimpleListContainer<E> {
 
     private int size = 0;
     private Node<E> first;
     private Node<E> last;
-    private int count = 0;
 
     public SimpleQueue() {
         first = new Node(null, null, last);
         last = new Node(first, null, null);
     }
-
-    public E poll() {
-        final Node<E> result = first.next;
-        return (result.item == null) ? null : helpForPoll(result);
-    }
-
-    public void push(E value) {
-
-    }
-
 
     private class Node<E> {
         E item;
@@ -42,62 +31,67 @@ public class SimpleQueue<E> implements Iterable<E> {
         }
     }
 
-
-
-
-    // метод извлекает иэлемент из головы очереди и возвращает его
-    // удаляет этот элемент или возвращает нул если очередь пустая
-
-    private E helpForPoll(Node<E> value) {
-        final E element = value.item;
-        first = new Node<>(null, null, first.next.next);
-        value.prev = null;
-        value.item = null;
-        value.next = null;
-        size--;
-        return element;
-    }
-
-    // метод вставляет элемент в конец очереди согласно логике
-    public boolean offer(E value) {
-        size++;
-        return true;
-    }
-
-    @Override
-    public boolean add(Object value) {
-        size++;
-        return true;
-    }
-
-    @Override
-    public E get(int index) {
-        Node<E> temp = first.next;
-        for (int i = 0; i < index; i++) {
-            temp = getNextElement(temp);
+    public void push(E value) {
+        final Node<E> lastNode = this.last;
+        final Node<E> firstNode = new Node<>(lastNode, value, null);
+        this.last = firstNode;
+        if (lastNode != null) {
+            lastNode.next = firstNode;
+        } else {
+            this.first = firstNode;
         }
-        return temp.item;
+        size++;
     }
 
-    private Node<E> getNextElement(Node<E> value) {
-        return value.next;
+    public E poll() {
+        final Node<E> result = first.next;
+
     }
+
 
     @Override
     public Iterator iterator() {
-        return new Iterator() {
-            @Override
-            public boolean hasNext() {
-                return count < size;
-            }
-
-            @Override
-            public E next() {
-                if (count == size) {
-                    throw new NoSuchElementException();
-                }
-                return get(count++);
-            }
-        };
+        return null;
     }
 }
+
+/*
+    Используя контейнер на базе связанного списка создать контейнер Stack и Queue.
+
+public class SimpleStack<T> {
+    public <T> poll()
+
+    public void push(T value);
+}
+
+public class SimpleQueue<T> {
+    public <T> poll()
+
+    public void push(T value);
+}
+
+    метод poll - должен возвращать значение и удалять его из коллекции. При реализации очереди и стека можете использовать коллекции, которые вы реализовывали в предыдущих заданиях.
+
+        Описание Queue - очередь. Описывается FIFO - first input first output.
+
+        То есть, первый зашел и первый вышел. Например.
+
+        push(1);
+        push(2);
+        push(3);
+
+        poll() - 1
+        poll() - 2
+        poll() - 3
+
+        Описание Stack - стек. Описывается LIFO - last input first output.
+
+        То есть, последний зашел и первый вышел. Например.
+
+        push(1);
+        push(2);
+        push(3);
+
+        poll() - 3
+        poll() - 2
+        poll() - 1*/
