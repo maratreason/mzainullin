ec738d3107290c26a35cebbc9c52bd23e710d945
maratreason <maratreason@mail.ru>
2018-06-08 13:10:45 +0300

    1. Реализовать коллекцию Set на массиве

--- chapter_003/src/main/java/ru.mzainullin/iterators/set/SimpleSet.java
+++ chapter_003/src/main/java/ru.mzainullin/iterators/set/SimpleSet.java
@@ -10,18 +10,8 @@ import java.util.Iterator;
  */
 public class SimpleSet<E> implements Iterable<E> {
 
-    private final int INIT_SIZE = 16;
-    private final int CUT_RATE = 4;
-    private Object[] array = new Object[INIT_SIZE];
-    private int pointer = 0;
+    private DynamicArrayList<E> container = new DynamicArrayList<E>();
 
-    DynamicArrayList<E> container = new DynamicArrayList<E>();
-
-    /**
-     * Метод добавления элемента
-     * @param element - новый элемент.
-     * @return - true / false.
-     */
     public boolean add(E element) {
         for (E check : this.container) {
             if (check.equals(element)) {
@@ -32,47 +22,8 @@ public class SimpleSet<E> implements Iterable<E> {
         return true;
     }
 
-    /**
-     * Вспомогательный метод для масштабирования.
-     * @param newLength - новая копия массива после изменений.
-     */
-    private void resize(int newLength) {
-        Object[] newArray = new Object[newLength];
-        System.arraycopy(array, 0, newArray, 0, pointer);
-        array = newArray;
-    }
-
-    /**
-     * Метод возвращает элемент списка по индексу.
-     * @param index - индекс искомого элемента.
-     * @return - возвращает индекс найденного элемента.
-     */
-    public E get(int index) {
-        return (E) this.array[index];
-    }
-
     @Override
     public Iterator<E> iterator() {
-        Iterator<E> it = new Iterator<E>() {
-
-            private int currentIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                return currentIndex < CUT_RATE && array[currentIndex] != null;
-            }
-
-            @Override
-            public E next() {
-                return (E) array[currentIndex++];
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-        return it;
+        return  this.container.iterator();
     }
 }
-
--- chapter_003/src/test/java/ru/mzainullin/iterators/set/SimpleSetTest.java
+++ chapter_003/src/test/java/ru/mzainullin/iterators/set/SimpleSetTest.java
@@ -1,9 +1,12 @@
 package ru.mzainullin.iterators.set;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import ru.mzainullin.iterators.list.DynamicArrayList;
 
+
+import java.util.Iterator;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -14,34 +17,39 @@ import static org.junit.Assert.*;
  */
 public class SimpleSetTest {
 
-//    SimpleSet<String> set = new SimpleSet<>();
-
-    private DynamicArrayList<String> list;
+    private SimpleSet<Integer> simpleSet;
+    private Iterator<Integer> itr;
 
     @Before
     public void beforeTest() {
-        list = new DynamicArrayList<>();
-        list.add("first");
-        list.add("second");
-        list.add("third");
-        list.add("fourth");
-
-        for (String s : list) {
-            System.out.println(s);
-        }
+        simpleSet = new SimpleSet<>();
+        itr = simpleSet.iterator();
     }
+
 
     @Test
-    public void whenAddElementToSet() {
-        list.add("second");
-
-        System.out.println("\nПосле добавления копии");
-        for (String s : list) {
-            System.out.println(s);
-        }
-
-        assertThat(list.get(4), is("third"));
+    public void whenAddOneThenTrue() {
+        assertThat(simpleSet.add(1), Matchers.is(true));
     }
 
 
+    @Test
+    public void whenAddOneOneTwoThenOneTwo() {
+        simpleSet.add(1);
+        assertThat(simpleSet.add(1), Matchers.is(false));
+        simpleSet.add(2);
+        assertThat(itr.next(), Matchers.is(1));
+        assertThat(itr.next(), Matchers.is(2));
+    }
+
+
+    @Test
+    public void whenAddOneTwoThenOneTwoTrueFalse() {
+        simpleSet.add(1);
+        simpleSet.add(2);
+        assertThat(itr.next(), Matchers.is(1));
+        assertThat(itr.hasNext(), Matchers.is(true));
+        assertThat(itr.next(), Matchers.is(2));
+        assertThat(itr.hasNext(), Matchers.is(false));
+    }
 }
