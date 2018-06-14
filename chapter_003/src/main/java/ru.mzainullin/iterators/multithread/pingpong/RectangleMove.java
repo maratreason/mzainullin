8cba71975738199b9782942fafb8ca750d160663
maratreason <maratreason@mail.ru>
2018-06-15 00:59:17 +0300

    1. Пин-понг

--- chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/RectangleMove.java
+++ chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/RectangleMove.java
@@ -2,31 +2,36 @@ package ru.mzainullin.iterators.multithread.pingpong;
 
 import javafx.scene.shape.Rectangle;
 
-public class RectangleMove implements Runnable {
-    private final Rectangle rect;
 
+public class RectangleMove implements Runnable {
+
+    private final Rectangle rect;
+    int count = 0;
     public RectangleMove(Rectangle rect) {
         this.rect = rect;
     }
 
     @Override
     public void run() {
+
         while (true) {
-            if (this.rect.getX() == 300) {
-                try {
-                    Thread.sleep(0);
-                    new Thread () {
-                        @Override
-                        public void run() {
-                            new Thread(new RectangleMoveBack(rect)).start();
-                        }
-                    }.start();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+            if (this.rect.getX() < 300) {
+                this.rect.setX(this.rect.getX() + 1);
+            } else if (this.rect.getX() == 300) {
+                while (this.rect.getX() > 0) {
+                    if (this.rect.getX() > 0) {
+                        this.rect.setX(this.rect.getX() - 1);
+                    } else {
+                        this.rect.setX(this.rect.getX() + 1);
+                    }
+
                 }
             }
 
-            this.rect.setX(this.rect.getX() + 1);
+
+
+
             try {
                 Thread.sleep(20);
             } catch (InterruptedException e) {
@@ -34,6 +39,7 @@ public class RectangleMove implements Runnable {
             }
         }
 
-    }
 
+    }
 }
+
