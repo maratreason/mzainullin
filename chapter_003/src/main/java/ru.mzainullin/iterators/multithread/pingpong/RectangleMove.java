0d4b0fdb9d8634c2b26adb0ca40d264f7ec2098d
maratreason <maratreason@mail.ru>
2018-06-18 20:49:24 +0300

    1. Пин-понг.

--- chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/RectangleMove.java
+++ chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/RectangleMove.java
@@ -5,8 +5,11 @@ import javafx.scene.shape.Rectangle;
 
 public class RectangleMove implements Runnable {
 
+    private static final double CHANGE = -1;
+    private double moveX = 1D;
+    private double moveY = 0.5D;
+
     private final Rectangle rect;
-    int count = 0;
     public RectangleMove(Rectangle rect) {
         this.rect = rect;
     }
@@ -15,30 +18,35 @@ public class RectangleMove implements Runnable {
     public void run() {
 
         while (true) {
+            if (this.rect.getX() >= 300D || this.rect.getX() <= 0D) {
+                this.moveX *= CHANGE;
+            }
 
-            if (this.rect.getX() < 300) {
-                this.rect.setX(this.rect.getX() + 1);
-            } else if (this.rect.getX() == 300) {
-                while (this.rect.getX() > 0) {
-                    if (this.rect.getX() > 0) {
-                        this.rect.setX(this.rect.getX() - 1);
+            if (this.rect.getY() >= 300D || this.rect.getY() <= 0D) {
+                this.moveY *= CHANGE;
+            }
+
+            if (this.rect.getX() < 300 || this.rect.getY() < 300) {
+                this.rect.setX(this.rect.getX() + moveX);
+                this.rect.setY(this.rect.getY() + moveY);
+            } else if (this.rect.getX() == 300 || this.rect.getY() == 300) {
+                while (this.rect.getX() > 0 || this.rect.getY() > 0) {
+                    if (this.rect.getX() > 0 || this.rect.getY() > 0) {
+                        this.rect.setX(this.rect.getX() - moveX);
+                        this.rect.setY(this.rect.getY() - moveY);
                     } else {
-                        this.rect.setX(this.rect.getX() + 1);
+                        this.rect.setX(this.rect.getX() + moveX);
+                        this.rect.setY(this.rect.getY() + moveY);
                     }
-
                 }
             }
 
-
-
-
             try {
-                Thread.sleep(20);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
 
     }
 }
