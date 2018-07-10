8ed7870ef95c37ec7fe4e998bfc238769c20dfe8
maratreason <maratreason@mail.ru>
2018-07-10 16:54:52 +0300

    1. класс Count и метод int incremant()

---
+++ chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/monitor/Count.java
@@ -0,0 +1,19 @@
+package ru.mzainullin.iterators.multithread.pingpong.monitor;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
