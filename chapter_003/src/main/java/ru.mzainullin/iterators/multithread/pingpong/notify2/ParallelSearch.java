3cff8c4b69cf84f522d67c1c2da03e5cfbdd32c0
maratreason <maratreason@mail.ru>
2018-10-11 00:18:41 +0300

    2. Обеспечить остановку потребителя.

---
+++ chapter_003/src/main/java/ru.mzainullin/iterators/multithread/pingpong/notify2/ParallelSearch.java
@@ -0,0 +1,56 @@
+package ru.mzainullin.iterators.multithread.pingpong.notify2;
+
+import ru.mzainullin.iterators.multithread.pingpong.notify.ProducerCustomer;
+import ru.mzainullin.iterators.multithread.pingpong.notify.SimpleBlockingQueue;
+
+/**
+ * @author Marat Zainullin
+ * @version 1.0
+ * @since 09.10.2018
+ */
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+
+        queue.offer(15);
+        queue.offer(23);
+        queue.offer(27);
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                        if (queue.poll() == null) {
+                            break;
+                        }
+                    }
+                }
+        );
+
+        final Thread producer =  new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        );
+
+        consumer.start();
+        producer.start();
+
+    }
+}
